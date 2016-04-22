ed2b3af5032547e4f617b20826ff7c85eb81b7e7
Edgar Osorio <orkwizard@gmail.com>
2016-04-22 18:34:40 -0500

    Data y MessageEntities

---
+++ AgileTrack/src/com/spheres/agiletrack/entities/Data.java
@@ -0,0 +1,54 @@
+package com.spheres.agiletrack.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+
+/**
+ * The persistent class for the Data database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Data.findAll", query="SELECT d FROM Data d")
+public class Data implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String field;
+
+	//bi-directional many-to-one association to Message
+	@ManyToOne
+	@JoinColumn(name="message_id")
+	private Message message;
+
+	public Data() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getField() {
+		return this.field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	public Message getMessage() {
+		return this.message;
+	}
+
+	public void setMessage(Message message) {
+		this.message = message;
+	}
+
+}
---
+++ AgileTrack/src/com/spheres/agiletrack/entities/Message.java
@@ -0,0 +1,102 @@
+package com.spheres.agiletrack.entities;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
+
+
+/**
+ * The persistent class for the Message database table.
+ * 
+ */
+@Entity
+@NamedQuery(name="Message.findAll", query="SELECT m FROM Message m")
+public class Message implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="command_id")
+	private int commandId;
+
+	@Column(name="message_header")
+	private String messageHeader;
+
+	@Column(name="protocol_header")
+	private String protocolHeader;
+
+	@Column(name="unit_id")
+	private String unitId;
+
+	//bi-directional many-to-one association to Data
+	@OneToMany(mappedBy="message")
+	private List<Data> data;
+
+	public Message() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCommandId() {
+		return this.commandId;
+	}
+
+	public void setCommandId(int commandId) {
+		this.commandId = commandId;
+	}
+
+	public String getMessageHeader() {
+		return this.messageHeader;
+	}
+
+	public void setMessageHeader(String messageHeader) {
+		this.messageHeader = messageHeader;
+	}
+
+	public String getProtocolHeader() {
+		return this.protocolHeader;
+	}
+
+	public void setProtocolHeader(String protocolHeader) {
+		this.protocolHeader = protocolHeader;
+	}
+
+	public String getUnitId() {
+		return this.unitId;
+	}
+
+	public void setUnitId(String unitId) {
+		this.unitId = unitId;
+	}
+
+	public List<Data> getData() {
+		return this.data;
+	}
+
+	public void setData(List<Data> data) {
+		this.data = data;
+	}
+
+	public Data addData(Data data) {
+		getData().add(data);
+		data.setMessage(this);
+
+		return data;
+	}
+
+	public Data removeData(Data data) {
+		getData().remove(data);
+		data.setMessage(null);
+
+		return data;
+	}
+
+}
